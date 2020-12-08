11993d700e8c7d73d12bfb8ab06800e38d54e011
Arijit Kundu <kunduarijit55@gmail.com>
2020-12-08 17:03:10 +0530

    Youtube suggested videos turned off :construction:

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
 var comments = document.getElementById("sections");
 comments.style.display = "none";
+
+//items is a list
+// var items = document.getElementsByTagName("ytd-compact-video-renderer");
+// while (items.length != 0) {
+//   for (var i = 0; i < items.length; i++){
+//     items[i].style.display = "none";
+//   } 
+// }
